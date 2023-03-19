2a1c95cc58c3f330988eb18651c4395847dceaee
RRoxxxsii <mishabur38@yandex.com>
2023-03-19 18:56:43 +0300

    Modernize login function, added exceptions

--- mysite/regester/urls.py
+++ mysite/regester/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('regester/', regester, name='regester'),
-    path('login/', login, name='login'),
+    path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout_user')
 ]
--- mysite/regester/views.py
+++ mysite/regester/views.py
@@ -1,13 +1,34 @@
-from django.contrib.auth import logout
+from django.contrib.auth import logout, authenticate, login
 from django.shortcuts import render, redirect
+from regester.forms import LoginForm
+from django.views.generic import TemplateView
+
+def login_user(request):
+    context = {'login_form': LoginForm()}
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('index')
+            else:
+                context = {
+                    'login_form': login_form,
+                    'attention': f'The user with {username} and password was not found'
+                }
+        else:
+            context = {
+                'login_form': login_form,
+            }
+
+    return render(request, 'regester/login.html', context)
 
 
-def login(request):
-    return render(request, 'regester/login.html')
-
-
-def regester(request):
-    return render(request, 'regester/regester.html')
+class RegisterView(TemplateView):
+    template_name = 'regester/regester.html'
 
 
 def logout_user(request):
