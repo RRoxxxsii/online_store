6e695db88470c15e92c790404acb0d7209ad458a
RRoxxxsii <mishabur38@yandex.com>
2023-03-21 11:52:26 +0300

    Create database

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,6 +1,10 @@
+from _decimal import Decimal
 from django.contrib.auth.models import User
-from django.db import models
-
+from django.db import models, transaction
+from django.db.models import Sum
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.utils import timezone
 
 
 class Payment(models.Model):
@@ -9,26 +13,82 @@ class Payment(models.Model):
     amount = models.DecimalField(max_digits=15, decimal_places=2, blank=True, null=True)
     comment = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return f'{self.user} - {self.amount}'
+
+    class Meta:
+        ordering = ['pk']
+
+    @staticmethod
+    def get_balanse(user: User):
+        amount = Payment.objects.filter(user=user).aggregate(Sum('amount'))['amount__sum']
+
+        return amount or Decimal(0)
+
 class Order(models.Model):
-    STATUS_CARD = '1_cart'
+    STATUS_CART = '1_cart'
     STATUS_WAITING_FOR_PAYMENT = '2_waiting_for_payment'
     STATUS_PAID = '3_paid'
     STATUS_CHOICES = [
-        (STATUS_CARD, 'cart'),
+        (STATUS_CART, 'cart'),
         (STATUS_WAITING_FOR_PAYMENT, 'waiting_for_payment'),
         (STATUS_PAID, 'paid')
     ]
     date_created = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=35, choices=STATUS_CHOICES, default=STATUS_CARD)
+    status = models.CharField(max_length=35, choices=STATUS_CHOICES, default=STATUS_CART)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=20, decimal_places=2, blank=True, null=True)
     creation_time = models.DateTimeField(auto_now_add=True)
     payment = models.ForeignKey(Payment, on_delete=models.PROTECT, blank=True, null=True)
 
+    def __str__(self):
+        return f'{self.user} - {self.amount} - {self.status}'
+
+    class Meta:
+        ordering = ['pk']
+
+    @staticmethod
+    def get_card(user: User):
+        cart = Order.objects.filter(user=user, status=Order.STATUS_CART).first()
+        if cart and (timezone.now() - cart.creation_time).days > 7:
+            cart.delete()
+            cart = None
+
+        if not cart:
+            cart = Order.objects.create(user=user, status=Order.STATUS_CART, amount=0)
+
+         return cart
+
+    def get_amount(self):
+        amount = Decimal(0)
+        for item in self.orderitem_set.all():
+            amount += item.amount
+        return amount
+
+    def make_order(self):
+        items = self.orderitem_set.all()
+        if items and self.status == Order.STATUS_CART:
+            self.status = Order.STATUS_WAITING_FOR_PAYMENT
+            self.save()
+            auto_payment_unpaid_orders(self.user)
+
+    @staticmethod
+    def get_amount_of_unpaid_orders(user: User):
+        amount = Order.objects.filter(user=user, status=Order.STATUS_WAITING_FOR_PAYMENT,
+                                      ).aggregate(Sum('amount'))['amount__sum']
+
+        return amount or Decimal(0)
+
 
 
 class ProductCategory(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['pk']
 
 
 class Product(models.Model):
@@ -39,6 +99,12 @@ class Product(models.Model):
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     image_url = models.URLField()
 
+    def __str__(self):
+        return f'{self.name} - {self.price}'
+
+    class Meta:
+        ordering = ['pk']
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
@@ -46,3 +112,44 @@ class OrderItem(models.Model):
     quantity = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     discount = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f'{self.product} - {self.price}'
+
+    class Meta:
+        ordering = ['pk']
+
+    def amount(self):
+        return self.quantity * (self.price - self.discount)
+
+
+@transaction.atomic()
+def auto_payment_unpaid_orders(user: User):
+    unpaid_orders = Order.objects.filter(user=user,
+                                         status=Order.STATUS_WAITING_FOR_PAYMENT)
+    for order in unpaid_orders:
+        if Payment.get_balanse(user) < order.amount:
+            break
+        order.payment = Payment.objects.all().last()
+        order.status = Order.STATUS_PAID
+        order.save()
+        Payment.objects.create(user=user, amount=-order.amount)
+
+
+
+@receiver(post_save, sender=OrderItem)
+def recalculate_order_amount_after_save(sender, instance, **kwargs):
+    order = instance.order
+    order.amount = order.get_amount()
+    order.save()
+
+@receiver(post_delete, sender=OrderItem)
+def recalculate_order_amount_after_delete(sender, instance, **kwargs):
+    order = instance.order
+    order.amount = order.get_amount()
+    order.save()
+
+@receiver(post_delete, sender=Payment)
+def auto_payment(sender, instance, **kwargs):
+    user = instance.user
+    auto_payment_unpaid_orders(user)
--- mysite/main/tests.py
+++ mysite/main/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+
+class TestDataBase(TestCase):
+    fixtures = [
+        'main/fixtures/data.json'
+    ]
+
+    def setUp(self) -> None:
+        self.user = User.objects.get(username='root')
+
+    def test_all_data(self):
+        self.assertGreater(Product.objects.all().count(), 0)
+        self.assertGreater(Order.objects.all().count(), 0)
+        self.assertGreater(ProductCategory.objects.all().count(), 0)
+        self.assertGreater(User.objects.all().count(), 0)
+        self.assertGreater(OrderItem.objects.all().count(), 0)
